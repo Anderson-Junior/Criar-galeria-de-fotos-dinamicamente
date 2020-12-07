3f04a50807188a411630fd7ad8e59a703ca41c73
Anderson-Junior <ander.lemos.jr@gmail.com>
2020-12-07 13:40:22 -0300

    Criando as primeiras rotas

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SecretariaController;
+use App\Http\Controllers\GaleriaController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ImagemController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+/*-----------------SECRETARIA---------------*/
+Route::get('/nova-secretaria', [SecretariaController::class, 'novaSecretaria'])->name('nova-secretaria');
+Route::post('/salvar-secretaria', [SecretariaController::class, 'salvarSecretaria']);
+Route::get('lista-secretaria', [SecretariaController::class, 'listaSecretaria'])->name('lista-secretaria');
+
+/*----------------GALERIA-------------------*/
+Route::get('/nova-galeria', [GaleriaController::class, 'novaGaleria'])->name('nova-galeria');
+Route::post('/salvar-galeria', [GaleriaController::class, 'salvarGaleria'])->name('salvar-galeria');
+Route::get('/lista-galeria', [GaleriaController::class, 'listaGaleria'])->name('lista-galeria');
+
+/*---------------categoria------------------*/
+Route::get('/nova-categoria', [CategoriaController::class, 'novaCategoria'])->name('nova-caregoria');
+Route::post('/salvar-categoria', [CategoriaController::class, 'salvarCategoria'])->name('salvar-categoria');
+Route::get('/lista-categoria', [CategoriaController::class, 'listaCategoria'])->name('lista-categoria');
